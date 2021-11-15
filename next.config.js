4a146b9367676523440952e33b44d9f1eae2916e
akndev0 <94179836+akndev0@users.noreply.github.com>
2021-11-15 14:58:00 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 module.exports={
   env:{
     BASE_URL:"https://gtrgrtclone.herokuapp.com",
-    ACCESS_TOKEN_SECRET: "123",
-    REFRESH_TOKEN_SECRET: "1234",
-    MONGO_URI:"mongodb+srv://new_user-123:HXINCPRahJmmyM1F@clustergtr.xufzw.mongodb.net/shop?retryWrites=true&w=majoritymongodb+srv://new_user-123:HXINCPRahJmmyM1F@clustergtr.xufzw.mongodb.net/shop?retryWrites=true&w=majoritymongodb+srv://new_user-123:HXINCPRahJmmyM1F@clustergtr.xufzw.mongodb.net/shop?retryWrites=true&w=majority"
+    ACCESS_TOKEN_SECRET: "",
+    REFRESH_TOKEN_SECRET: "",
+    MONGO_URI:""
 
 
   },
